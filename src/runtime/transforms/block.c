ae4fd4fa9a736ddb0ac65d5f73b06d515d54f974
Sig <sigfig@translunar.space>
2016-09-10 09:07:45 -0700

    toplevel transform for blocks

--- src/runtime/transforms/block.c
+++ src/runtime/transforms/block.c
@@ -1,9 +1,33 @@
 #include "runtime/transforms/common.h"
 
+// [todo] block introduces a scope
+
 LLVMValueRef tulip_runtime_transform_block(tulip_runtime_module* mod, char* name, tulip_runtime_ast_block block, tulip_runtime_context ctx) {
   if(ctx.level == top) {
-    // [?] without "module effects", is this an error case or should i wrap blocks in local defs
-    return NULL;
+
+    LLVMValueRef local_fn = LLVMAddFunction(mod->llvm_module, render_qualified_name((tulip_runtime_ast_name){NULL, 0, name}, mod), tulip_defn_type);
+
+    LLVMBasicBlockRef bb = LLVMAppendBasicBlock(local_fn, "");
+
+
+    tulip_runtime_context content_ctx = ctx;
+    content_ctx.block = bb;
+
+    LLVMValueRef ret;
+
+    for (unsigned int i = 0; i < block.length; i++) {
+      ret = tulip_runtime_transform_core(mod, name, block.statements[i], content_ctx);
+    }
+
+    LLVMBuilderRef b = LLVMCreateBuilder();
+    LLVMPositionBuilderAtEnd(b, bb);
+
+    LLVMBuildRet(b, ret);
+
+    LLVMDisposeBuilder(b);
+
+    return local_fn;
+
   } else {
 
     LLVMBasicBlockRef bb = LLVMAppendBasicBlock(LLVMGetBasicBlockParent(ctx.block), "");
