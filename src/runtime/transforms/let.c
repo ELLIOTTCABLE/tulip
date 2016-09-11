e23e4d7591c60e96a2947f58cc307655347a87d6
Sig <sigfig@translunar.space>
2016-09-11 11:45:49 -0700

    transform for let

--- src/runtime/transforms/let.c
+++ src/runtime/transforms/let.c
@@ -1,7 +1,27 @@
 #include "runtime/transforms/common.h"
 
+#include <assert.h>
+
 LLVMValueRef tulip_runtime_transform_let(tulip_runtime_module* mod, char* name, tulip_runtime_ast_let let, tulip_runtime_context ctx) {
-  // append to context basic block
-  //   evaluate rhs
-  //   evalute name_set(lhs, rhs_result)
+
+    // [note] this case is almost certainly not valid
+    assert(ctx.level != top);
+
+    LLVMValueRef rhs = tulip_runtime_transform_core(mod, name, let.definition, ctx);
+
+    LLVMValueRef scope = LLVMConstIntToPtr(LLVMConstInt(LLVMInt64Type(), (long long) ctx.local_scope, false), LLVMPointerType(LLVMVoidType(), 0));
+
+    LLVMBuilderRef b = LLVMCreateBuilder();
+    LLVMPositionBuilderAtEnd(b, ctx.block);
+
+    if (let.bind.modulePathLen > 0) {
+      // [note] what to do for assignment to qualified names?
+    } else {
+      LLVMBuildCall(b, mod->native_defs->local_scope_set, (LLVMValueRef[]){scope, LLVMConstString(let.bind.name, strlen(let.bind.name), false)}, 2, "");
+    }
+
+    // [note] lets are void and consuming their value is always invalid
+    LLVMValueRef poison = LLVMGetUndef(tulip_value_type);
+
+    return poison;
 }
