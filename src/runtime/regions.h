cf9b009acde73044814140826e9bb7227f40b66b
Sig <sigfig@translunar.space>
2016-09-12 11:28:35 -0700

    expanded region interface

--- src/runtime/regions.h
+++ src/runtime/regions.h
@@ -27,6 +27,8 @@ typedef struct value_region {
 } value_region;
 
 value_region*        init_value_region(unsigned int num_values);
+void                 free_value_region(value_region* r);
+
 void                 check_value_region(value_region* r);
 value_ref            region_insert_value(value_region* r, tulip_runtime_value v);
 void                 region_delete_value(value_region* r, value_ref v);
@@ -52,7 +54,11 @@ typedef struct module_region {
 } module_region;
 
 module_region*        init_module_region(unsigned int num_modules);
+void                  free_module_region(module_region* r);
+
 module_ref            region_insert_module(module_region* r, tulip_runtime_module m);
 void                  region_delete_module(module_region* r, module_ref m);
+
+tulip_runtime_module* region_query_module_by_id(module_region* r, module_ref m);
 tulip_runtime_module* region_query_module_by_name(const char* name);
 tulip_runtime_module* region_query_module_by_version(const char* name, tulip_runtime_module_version ver);
